d78354ae8dcafe0a8613cc4d89a4993d83296606
Philipp Reger <reger-p@users.noreply.github.com>
2020-04-26 16:20:33 +0200

    Create setup_fail2ban.sh

---
+++ setup_fail2ban.sh
@@ -0,0 +1,11 @@
+apt install fail2ban
+systemctl enable fail2ban
+cat << EOF > /etc/fail2ban/jail.local
+[sshd]
+enabled = true
+banaction = iptables-multiport
+maxretry = 10
+findtime = 300
+bantime = 86400
+systemctl restart fail2ban
+systemctl status fail2ban
