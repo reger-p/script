0d3ae8912cf5cb7c055a8b1bc3971e2af410a728
Philipp Reger <reger-p@users.noreply.github.com>
2020-04-26 16:20:55 +0200

    Update setup_fail2ban.sh

--- setup_fail2ban.sh
+++ setup_fail2ban.sh
@@ -1,11 +1,12 @@
 apt install fail2ban
 systemctl enable fail2ban
 cat << EOF > /etc/fail2ban/jail.local
-[sshd]
-enabled = true
-banaction = iptables-multiport
-maxretry = 10
-findtime = 300
-bantime = 86400
+  [sshd]
+  enabled = true
+  banaction = iptables-multiport
+  maxretry = 10
+  findtime = 300
+  bantime = 86400
+EOF
 systemctl restart fail2ban
 systemctl status fail2ban
